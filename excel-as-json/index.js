67698c4c943d27b1247195f38f3d1ac5e9f6983b
Ishan Shams <ishams@techtrend.us>
2017-05-10 09:52:57 -0400

    first draft
    basic architecture complete

---
+++ excel-as-json/index.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+convertExcel = require('excel-as-json').processFile;
+
+var src = "C:/Users/Ishan/node_modules/excel-as-json/src/jsonExample.xlsx";
+var dest = "C:/Users/Ishan/node_modules/excel-as-json/dest/"
+
+var result = convertExcel(src,undefined,false,function(err,data) {
+    if(err){
+        console.log(err); 
+        console.log(data);
+    } else {
+        for (var i = 0; i < data.length; i++) {
+            var json = JSON.stringify(data[i], null, 4);
+            //console.log(json)
+            var path = dest + "result_" + i + ".json"; 
+            fs.writeFile(path, json, 'utf8', function(err,data) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+    }
+});
